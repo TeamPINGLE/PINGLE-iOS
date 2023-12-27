cde596e2dda8d6640ba81352dfa19175784c7d11
정채은 <cce523@hanmail.net>
2023-12-27 14:31:25 +0900

    [Setting] 프로젝트 생성

---
+++ PINGLE-iOS/PINGLE-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PINGLE-iOS
+//
+//  Created by 정채은 on 12/27/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

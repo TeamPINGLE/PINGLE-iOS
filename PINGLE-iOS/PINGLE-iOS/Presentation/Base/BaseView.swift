d54b6ff3ab2535984010926dbc3f53ebfe26c463
정채은 <cce523@hanmail.net>
2023-12-27 14:56:51 +0900

    Feat [#1] BaseView / BaseViewController 추가

--- PINGLE-iOS/PINGLE-iOS/Presentation/Base/BaseView.swift
+++ PINGLE-iOS/PINGLE-iOS/Presentation/Base/BaseView.swift
@@ -5,4 +5,32 @@
 //  Created by 정채은 on 12/27/23.
 //
 
-import Foundation
+import UIKit
+
+class BaseView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUI()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setUI() {
+        setStyle()
+        setLayout()
+    }
+    
+    func setStyle() {
+        
+    }
+    
+    func setLayout() {
+        
+    }
+    
+}
+
--- PINGLE-iOS/PINGLE-iOS/Presentation/Base/BaseViewController.swift
+++ PINGLE-iOS/PINGLE-iOS/Presentation/Base/BaseViewController.swift
@@ -5,4 +5,53 @@
 //  Created by 정채은 on 12/27/23.
 //
 
-import Foundation
+import UIKit
+
+class BaseViewController: UIViewController {
+    
+    // MARK: Properties
+    
+    lazy private(set) var className: String = {
+        return type(of: self).description().components(separatedBy: ".").last ?? ""
+    }()
+    
+    // MARK: Initializing
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    deinit {
+        print("DEINIT: \(className)")
+    }
+    
+    // MARK: Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUI()
+        setDelegate()
+    }
+    
+    // MARK: UI
+    func setUI() {
+        setStyle()
+        setLayout()
+    }
+    
+    func setStyle() {
+        view.backgroundColor = .white
+    }
+    
+    func setLayout() {
+        
+    }
+    
+    func setDelegate() {
+        
+    }
+}

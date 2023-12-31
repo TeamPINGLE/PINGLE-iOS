2c17a5aa184ab2efb267e4b9e9f59fa375092a5e
chaentopia <cce523@hanmail.net>
2023-12-31 16:39:39 +0900

    Add [#1] design system - font 추가

--- PINGLE-iOS/PINGLE-iOS/Global/Resources/Font.swift
+++ PINGLE-iOS/PINGLE-iOS/Global/Resources/Font.swift
@@ -5,4 +5,80 @@
 //  Created by 정채은 on 12/27/23.
 //
 
-import Foundation
+import UIKit
+
+extension UIFont {
+
+  class var titleTitleSemi32: UIFont {
+    return UIFont(name: "SUIT-SemiBold", size: 32.0)!
+  }
+
+  class var titleTitleSemi30: UIFont {
+    return UIFont(name: "SUIT-SemiBold", size: 30.0)!
+  }
+
+  class var titleTitleSemi24: UIFont {
+    return UIFont(name: "SUIT-SemiBold", size: 24.0)!
+  }
+
+  class var titleTitleSemi20: UIFont {
+    return UIFont(name: "SUIT-SemiBold", size: 20.0)!
+  }
+
+  class var subtitleSubBold18: UIFont {
+    return UIFont(name: "SUIT-Bold", size: 18.0)!
+  }
+
+  class var subtitleSubSemi18: UIFont {
+    return UIFont(name: "SUIT-SemiBold", size: 18.0)!
+  }
+
+  class var bodyBodyMed18: UIFont {
+    return UIFont(name: "SUIT-Medium", size: 18.0)!
+  }
+
+  class var subtitleSubSemi16: UIFont {
+    return UIFont(name: "SUIT-SemiBold", size: 16.0)!
+  }
+
+  class var bodyBodyMed16: UIFont {
+    return UIFont(name: "SUIT-Medium", size: 16.0)!
+  }
+
+  class var bodyBodySemi14: UIFont {
+    return UIFont(name: "SUIT-SemiBold", size: 14.0)!
+  }
+
+  class var bodyBodyMed14: UIFont {
+    return UIFont(name: "SUIT-Medium", size: 14.0)!
+  }
+
+  class var subtitleSubExtra12: UIFont {
+    return UIFont(name: "SUIT-ExtraBold", size: 12.0)!
+  }
+
+  class var captionCapBold12: UIFont {
+    return UIFont(name: "SUIT-Bold", size: 12.0)!
+  }
+
+  class var captionCapSemi12: UIFont {
+    return UIFont(name: "SUIT-SemiBold", size: 12.0)!
+  }
+
+  class var captionCapMed12: UIFont {
+    return UIFont(name: "SUIT-Medium", size: 12.0)!
+  }
+
+  class var captionCapBold10: UIFont {
+    return UIFont(name: "SUIT-Bold", size: 10.0)!
+  }
+
+  class var captionCapSemi10: UIFont {
+    return UIFont(name: "SUIT-SemiBold", size: 10.0)!
+  }
+
+  class var captionCapMed10: UIFont {
+    return UIFont(name: "SUIT-Medium", size: 10.0)!
+  }
+
+}

1de9e8ee37e8d71083cbb759319cab8175b94834
chaentopia <cce523@hanmail.net>
2023-12-31 16:39:50 +0900

    Add [#1] design system - color 추가

--- PINGLE-iOS/PINGLE-iOS/Global/Resources/Color.swift
+++ PINGLE-iOS/PINGLE-iOS/Global/Resources/Color.swift
@@ -5,4 +5,78 @@
 //  Created by 정채은 on 12/27/23.
 //
 
-import Foundation
+import UIKit
+
+/// 사용법
+/// label.textColor = .mainPingleGreen
+
+extension UIColor {
+
+  @nonobjc class var black: UIColor {
+    return UIColor(red: 8.0 / 255.0, green: 9.0 / 255.0, blue: 9.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var white: UIColor {
+    return UIColor(white: 254.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var mainPingleGreen: UIColor {
+    return UIColor(red: 183.0 / 255.0, green: 1.0, blue: 29.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var subPingleOrange: UIColor {
+    return UIColor(red: 1.0, green: 146.0 / 255.0, blue: 84.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var subPingleYellow: UIColor {
+    return UIColor(red: 242.0 / 255.0, green: 1.0, blue: 94.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var grayscaleG01: UIColor {
+    return UIColor(red: 237.0 / 255.0, green: 240.0 / 255.0, blue: 244.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var grayscaleG02: UIColor {
+    return UIColor(red: 219.0 / 255.0, green: 226.0 / 255.0, blue: 233.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var grayscaleG03: UIColor {
+    return UIColor(red: 202.0 / 255.0, green: 209.0 / 255.0, blue: 217.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var grayscaleG04: UIColor {
+    return UIColor(red: 183.0 / 255.0, green: 191.0 / 255.0, blue: 198.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var grayscaleG05: UIColor {
+    return UIColor(red: 160.0 / 255.0, green: 170.0 / 255.0, blue: 178.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var grayscaleG06: UIColor {
+    return UIColor(red: 127.0 / 255.0, green: 138.0 / 255.0, blue: 150.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var grayscaleG07: UIColor {
+    return UIColor(red: 98.0 / 255.0, green: 109.0 / 255.0, blue: 121.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var grayscaleG08: UIColor {
+    return UIColor(red: 72.0 / 255.0, green: 83.0 / 255.0, blue: 92.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var grayscaleG09: UIColor {
+    return UIColor(red: 54.0 / 255.0, green: 62.0 / 255.0, blue: 69.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var grayscaleG10: UIColor {
+    return UIColor(red: 37.0 / 255.0, green: 41.0 / 255.0, blue: 48.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var grayscaleG11: UIColor {
+    return UIColor(red: 21.0 / 255.0, green: 24.0 / 255.0, blue: 27.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var systemRedAlert: UIColor {
+    return UIColor(red: 214.0 / 255.0, green: 57.0 / 255.0, blue: 47.0 / 255.0, alpha: 1.0)
+  }
+}

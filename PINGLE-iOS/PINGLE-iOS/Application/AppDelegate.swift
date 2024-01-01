f636aa4ac18ade111a4fbfd266f839133a36294a
chaentopia <cce523@hanmail.net>
2024-01-01 12:36:26 +0900

    Add [#1] NaverMap Cocoapods 패키지 추가
    추후 클라이언트 아이디 발급 예정

--- PINGLE-iOS/PINGLE-iOS/Application/AppDelegate.swift
+++ PINGLE-iOS/PINGLE-iOS/Application/AppDelegate.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 
+import NMapsMap
+
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -14,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        NMFAuthManager.shared().clientId = "YOUR_CLIENT_ID"
         return true
     }
 

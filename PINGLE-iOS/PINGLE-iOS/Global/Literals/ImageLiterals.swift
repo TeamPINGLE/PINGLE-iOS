36834a34194f49463cdbbe6671722e91bece08f6
Minsoo Kang <alstn38@naver.com>
2024-01-02 23:37:44 +0900

    Add [#8] 샘플이미지 에셋 추가

--- PINGLE-iOS/PINGLE-iOS/Global/Literals/ImageLiterals.swift
+++ PINGLE-iOS/PINGLE-iOS/Global/Literals/ImageLiterals.swift
@@ -14,6 +14,7 @@ enum ImageLiterals {
     
     enum OnBoarding {
         static var imgApplelogo: UIImage { .load(named: "imgApplelogo")}
+        static var imgSample: UIImage { .load(named: "imgSample")}
     }
     
     enum TabBar {
---
+++ PINGLE-iOS/PINGLE-iOS/Presentation/Onboarding/ViewController/OnboardingViewController.swift
@@ -0,0 +1,8 @@
+//
+//  OnboardingViewController.swift
+//  PINGLE-iOS
+//
+//  Created by 강민수 on 1/2/24.
+//
+
+import Foundation

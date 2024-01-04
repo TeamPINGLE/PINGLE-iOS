24b56b5578d1d1d9f39acf79b3fa7daf190b22a4
Minsoo Kang <alstn38@naver.com>
2024-01-05 01:35:40 +0900

    Feat [#21] PINGLETextFieldView 구현

---
+++ PINGLE-iOS/PINGLE-iOS/Global/Components/PINGLETextFieldView.swift
@@ -0,0 +1,76 @@
+//
+//  PINGLETextFieldView.swift
+//  PINGLE-iOS
+//
+//  Created by 강민수 on 1/4/24.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class PINGLETextFieldView: BaseView {
+
+    // MARK: Property
+    private let titleLabel = UILabel()
+    let searchTextField = UITextField()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    init(titleLabel: String, explainLabel: String) {
+        super.init(frame: .zero)
+        self.titleLabel.text = titleLabel
+        self.searchTextField.attributedPlaceholder = NSAttributedString(
+            string: explainLabel,
+            attributes: [
+                .font: UIFont.bodyBodyMed14,
+                .foregroundColor: UIColor.grayscaleG07
+            ]
+        )
+    }
+
+    // MARK: UI
+    override func setStyle() {
+        self.do {
+            $0.backgroundColor = .grayscaleG10
+            $0.layer.cornerRadius = 12.adjusted
+            $0.layer.backgroundColor = UIColor.grayscaleG10.cgColor
+        }
+        
+        self.titleLabel.do {
+            $0.font = .captionCapSemi12
+            $0.textColor = .grayscaleG03
+        }
+        
+        self.searchTextField.do {
+            $0.font = .subtitleSubSemi16
+            $0.textColor = .white
+            $0.tintColor = .mainPingleGreen
+            $0.returnKeyType = .done
+            $0.enablesReturnKeyAutomatically = true
+        }
+    }
+    
+    override func setLayout() {
+        self.addSubviews(titleLabel, searchTextField)
+        
+        self.snp.makeConstraints {
+            $0.width.equalTo(327.adjustedWidth)
+            $0.height.equalTo(75.adjusted)
+        }
+        
+        titleLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(16.adjusted)
+            $0.leading.equalToSuperview().offset(18.adjusted)
+        }
+        
+        searchTextField.snp.makeConstraints {
+            $0.top.equalTo(self.titleLabel.snp.bottom).offset(4.adjusted)
+            $0.leading.trailing.equalToSuperview().inset(18.adjusted)
+            $0.bottom.equalToSuperview().inset(16.adjusted)
+        }
+    }
+}

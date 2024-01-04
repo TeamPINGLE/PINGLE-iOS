3fb5c046fd9d2af6919ee15f9f0b23435f4935a0
Minsoo Kang <alstn38@naver.com>
2024-01-04 21:57:12 +0900

    Chore [#12] 공백 수정

--- PINGLE-iOS/PINGLE-iOS/Presentation/Onboarding/ViewController/SearchOrganizationViewController.swift
+++ PINGLE-iOS/PINGLE-iOS/Presentation/Onboarding/ViewController/SearchOrganizationViewController.swift
@@ -198,9 +198,11 @@ extension SearchOrganizationViewController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
         return 0
     }
+    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
         return 0
     }
+    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return CGSize(width: 325.adjustedWidth , height: 88.adjustedHeight)
     }

f6f8a50bcdac6b02416a740213d85941e540849c
정채은 <cce523@hanmail.net>
2024-01-05 11:08:41 +0900

    Add [#23] asset, literals 추가

---
+++ PINGLE-iOS/PINGLE-iOS/Presentation/Home/View/HomeMapDetailView.swift
@@ -0,0 +1,52 @@
+//
+//  HomeMapDetailView.swift
+//  PINGLE-iOS
+//
+//  Created by 정채은 on 1/5/24.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class HomeMapDetailView: BaseView {
+
+    // MARK: - Variables
+    // MARK: Property
+    
+    // MARK: Component
+    let topBackgroundView = UIView()
+    let badgeImageView = UIView()
+    let titleLabel = UILabel()
+    let nameLabel = UILabel()
+    
+    let participantsView = UIView()
+    let participantsLabel = UILabel()
+    let currentParticipantsLabel = UILabel()
+    let slashLabel = UILabel()
+    let totalParticipantsLabel = UILabel()
+    
+    let separateView = UIView()
+    
+    let bottomBackgroundView = UIView()
+    let dateTimeTitleLabel = UILabel()
+    let dateLabel = UILabel()
+    let timeLabel = UILabel()
+    let locationTitleLabel = UILabel()
+    let locationLabel = UILabel()
+    
+    let talkButton = UIButton()
+    let participationButton = UIButton()
+
+    // MARK: - Function
+    // MARK: Style Helpers
+    override func setStyle() {
+        
+    }
+    
+    // MARK: Layout Helpers
+    override func setLayout() {
+        
+    }
+}

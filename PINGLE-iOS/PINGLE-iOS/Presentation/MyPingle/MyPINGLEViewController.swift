b05394520996477afa12001604b023d9431a8db3
정채은 <cce523@hanmail.net>
2024-01-06 22:21:45 +0900

    Feat [#41] 공사중 마이핑글탭 연결

--- PINGLE-iOS/PINGLE-iOS/Presentation/MyPingle/MyPINGLEViewController.swift
+++ PINGLE-iOS/PINGLE-iOS/Presentation/MyPingle/MyPINGLEViewController.swift
@@ -7,6 +7,27 @@
 
 import UIKit
 
+import SnapKit
+import Then
+
 final class MyPINGLEViewController: BaseViewController {
+    let fixView = FixView()
+    
+    override func setStyle() {
+        self.navigationController?.navigationBar.isHidden = true
+        self.view.backgroundColor = .grayscaleG11
+    }
+    
+    override func setLayout() {
+        let safeAreaHeight = view.safeAreaInsets.bottom
+        let tabBarHeight = tabBarController?.tabBar.frame.height ?? 60
+        
+        self.view.addSubviews(fixView)
+        fixView.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaInsets).offset(117.adjustedHeight)
+            $0.leading.trailing.equalToSuperview().inset(50.adjustedWidth)
+            $0.bottom.equalTo(safeAreaHeight).offset(-(tabBarHeight + 132.adjustedHeight))
+        }
+    }
 
 }

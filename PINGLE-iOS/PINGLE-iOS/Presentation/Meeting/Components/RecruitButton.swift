82ee4ecf2402b0f0bb7284870290d6572e3a6614
bangMinji98 <whoru9806@gmail.com>
2024-01-08 05:27:34 +0900

    Feat [#54] Recruitment의 +, - 버튼 활용위한 class 구현

--- PINGLE-iOS/PINGLE-iOS/Presentation/Meeting/Components/RecruitButton.swift
+++ PINGLE-iOS/PINGLE-iOS/Presentation/Meeting/Components/RecruitButton.swift
@@ -5,4 +5,56 @@
 //  Created by 방민지 on 1/8/24.
 //
 
-import Foundation
+import UIKit
+
+import SnapKit
+
+class RecruitButton: UIButton {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    init(title: String, buttonColor: UIColor, textColor: UIColor) {
+        super.init(frame: .zero)
+        commonInit()
+        setLayout()
+        self.backgroundColor = buttonColor
+        self.setTitleColor(textColor, for: .normal)
+        self.setTitle(title, for: .normal)
+    }
+    
+// MARK: - Function
+    private func commonInit() {
+        self.isEnabled = false
+        self.titleLabel?.font = .titleTitleSemi24
+        self.layer.cornerRadius = 10.adjusted
+    }
+
+// MARK: - UI
+    private func setLayout() {
+        self.snp.makeConstraints {
+            $0.width.equalTo(50.adjusted)
+            $0.height.equalTo(51.adjusted)
+        }
+    }
+}
+
+// MARK: - extension
+extension RecruitButton {
+    func activateButton() {
+        self.backgroundColor = .white
+        self.setTitleColor(.black, for: .normal)
+        self.isEnabled = true
+    }
+    
+    func disabledButton() {
+        self.backgroundColor = .grayscaleG08
+        self.setTitleColor(.grayscaleG09, for: .normal)
+        self.isEnabled = false
+    }
+}

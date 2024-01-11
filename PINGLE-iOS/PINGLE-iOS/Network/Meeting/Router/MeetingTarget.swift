da64a607f19582e1b49f6bae599d78e80920410c
bangMinji98 <whoru9806@gmail.com>
2024-01-12 05:45:47 +0900

    Network [#83]  네트워크 관련 파일 구현

--- PINGLE-iOS/PINGLE-iOS/Network/Meeting/Router/MeetingTarget.swift
+++ PINGLE-iOS/PINGLE-iOS/Network/Meeting/Router/MeetingTarget.swift
@@ -5,3 +5,48 @@
 //  Created by 방민지 on 1/12/24.
 //
 
+import Foundation
+
+import Alamofire
+
+enum MeetingTarget {
+    case searchPlace(_ queryDTO: SearchPlaceRequestQueryDTO)
+
+}
+
+extension MeetingTarget: TargetType {
+    var authorization: Authorization {
+        switch self {
+        case .searchPlace:
+            return .authorization
+        }
+    }
+    
+    var headerType: HTTPHeaderType {
+        switch self {
+        case .searchPlace:
+            return .hasToken
+        }
+    }
+    
+    var method: HTTPMethod {
+        switch self {
+        case .searchPlace:
+            return .get
+        }
+    }
+    
+    var path: String {
+        switch self {
+        case .searchPlace(let location):
+            return "/location"
+        }
+    }
+    
+    var parameters: RequestParams {
+        switch self {
+        case .searchPlace(let queryDTO):
+            return .requestQuery(queryDTO)
+        }
+    }
+}
--- PINGLE-iOS/PINGLE-iOS/Network/Meeting/Service/MeetingService.swift
+++ PINGLE-iOS/PINGLE-iOS/Network/Meeting/Service/MeetingService.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+
+protocol MeetingServiceProtocol {
+    func searchPlace(queryDTO: SearchPlaceRequestQueryDTO, completion: @escaping (NetworkResult<BaseResponse<[SearchPlaceResponseDTO]>>) -> Void)
+}
+
+final class MeetingService: APIRequestLoader<MeetingTarget>, MeetingServiceProtocol{
+    func searchPlace(queryDTO: SearchPlaceRequestQueryDTO, completion: @escaping (NetworkResult<BaseResponse<[SearchPlaceResponseDTO]>>) -> Void) {
+        fetchData(target: .searchPlace(queryDTO), responseData: BaseResponse<[SearchPlaceResponseDTO]>.self, completion: completion)
+    }
+}

c539d2e51ab68c81337ccc0e0302a484e5c87239
chaentopia <cce523@hanmail.net>
2024-01-11 16:22:01 +0900

    Refactor [#71] collectionViewCell 구현

---
+++ PINGLE-iOS/PINGLE-iOS/Presentation/Home/Cell/HomeDetailCollectionViewCell.swift
@@ -0,0 +1,47 @@
+//
+//  HomeDetailCollectionViewCell.swift
+//  PINGLE-iOS
+//
+//  Created by 정채은 on 1/11/24.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class HomeDetailCollectionViewCell: UICollectionViewCell {
+    
+    // MARK: - Variables
+    // MARK: Constants
+    static let identifier = "HomeDetailCollectionViewCell"
+    
+    let mapDetailView = HomeMapDetailView()
+
+    // MARK: - Function
+    // MARK: LifeCycle
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: Layout Helpers
+    private func setUI() {
+        setStyle()
+        setLayout()
+    }
+    
+    private func setStyle() { }
+    
+    private func setLayout() {
+        self.addSubviews(mapDetailView)
+        
+        mapDetailView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+}

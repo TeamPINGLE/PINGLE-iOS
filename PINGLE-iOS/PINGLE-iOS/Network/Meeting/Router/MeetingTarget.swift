189eeddb530453c6d940a78236b7295ce978bd7f
bangMinji98 <whoru9806@gmail.com>
2024-01-12 19:10:17 +0900

    Network [#86] 핑글개최 서버 통신을 위한 MeetingTarget, MeetingService 구현

--- PINGLE-iOS/PINGLE-iOS/Network/Meeting/Router/MeetingTarget.swift
+++ PINGLE-iOS/PINGLE-iOS/Network/Meeting/Router/MeetingTarget.swift
@@ -6,3 +6,46 @@
 //
 
 import Foundation
+
+import Alamofire
+
+enum MeetingTarget {
+    case makeMeeting(_ bodyDTO: MakeMeetingRequestBodyDTO)
+}
+
+extension MeetingTarget: TargetType {
+    var authorization: Authorization {
+        switch self {
+        case .makeMeeting:
+            return .authorization
+        }
+    }
+    
+    var headerType: HTTPHeaderType {
+        switch self {
+        case .makeMeeting:
+            return .teamId
+        }
+    }
+    
+    var method: HTTPMethod {
+        switch self {
+        case .makeMeeting:
+            return .post
+        }
+    }
+    
+    var path: String {
+        switch self {
+        case .makeMeeting:
+            return "/meetings"
+        }
+    }
+    
+    var parameters: RequestParams {
+        switch self {
+        case .makeMeeting(let bodyDTO):
+            return .requestWithBody(bodyDTO)
+        }
+    }
+}
--- PINGLE-iOS/PINGLE-iOS/Network/Meeting/Service/MeetingService.swift
+++ PINGLE-iOS/PINGLE-iOS/Network/Meeting/Service/MeetingService.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+
+protocol MeetingServiceProtocol {
+    func makeMeeting(bodyDTO: MakeMeetingRequestBodyDTO, completion: @escaping (NetworkResult<BaseResponse<String?>>) -> Void)
+}
+
+final class MeetingService: APIRequestLoader<MeetingTarget>, MeetingServiceProtocol {
+    func makeMeeting(bodyDTO: MakeMeetingRequestBodyDTO, completion: @escaping (NetworkResult<BaseResponse<String?>>) -> Void) {
+        let headers = "\(KeychainHandler.shared.userGroup[0].id)" 
+        fetchData(target: .makeMeeting(bodyDTO), responseData: BaseResponse<String?>.self, completion: completion)
+    }
+}

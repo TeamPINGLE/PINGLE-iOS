77ae9b4fb99ecfd786873a7e2f12370f359724ce
Minsoo Kang <alstn38@naver.com>
2024-01-17 21:12:11 +0900

    Chore [#121] SplashViewController 주석 추가

--- PINGLE-iOS/PINGLE-iOS/Presentation/Splash/SplashViewController.swift
+++ PINGLE-iOS/PINGLE-iOS/Presentation/Splash/SplashViewController.swift
@@ -21,9 +21,7 @@ final class SplashViewController: BaseViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 1.5) {
-            self.branchProcessing()
-        }
+        branchProcessing()
     }
     
     override func setStyle() {
@@ -50,15 +48,18 @@ final class SplashViewController: BaseViewController {
     }
     
     func branchProcessing() {
-        if KeychainHandler.shared.accessToken.isEmpty {
-            /// 어세스 토큰 없는 경우 - 애플 계정을 통한 회원가입을 한 적이 없는 경우
-            changeRootViewController(rootViewController: loginViewController)
-        } else {
-            /// 어세스 토큰이 있는 경우 - 애플 계정을 통한 회원가입을 한 적이 있는 경우 : 가입한 단체가 있는지 확인하는 통신 코드 구현
-            getUserInfo()
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 1.5) {
+            if KeychainHandler.shared.accessToken.isEmpty {
+                /// 어세스 토큰 없는 경우 - 애플 계정을 통한 회원가입을 한 적이 없는 경우
+                self.changeRootViewController(rootViewController: self.loginViewController)
+            } else {
+                /// 어세스 토큰이 있는 경우 - 애플 계정을 통한 회원가입을 한 적이 있는 경우 : 가입한 단체가 있는지 확인하는 통신 코드 구현
+                self.getUserInfo()
+            }
         }
     }
     
+    // MARK: Network Function
     func getUserInfo() {
         NetworkService.shared.onboardingService.userInfo { [weak self] response in
             guard let self = self else { return }
@@ -69,8 +70,10 @@ final class SplashViewController: BaseViewController {
                     KeychainHandler.shared.userGroup = groups
                 }
                 if KeychainHandler.shared.userGroup.isEmpty {
+                    /// 유저가 가입한 단체가 없는 경우 - Onboarding 화면으로 이동하여 단체에 가입하도록 유도
                     self.changeRootViewController(rootViewController: self.onboardingViewController)
                 } else {
+                    /// 유저가 가입한 단체가 있는 경우 - 메인화면으로 이동
                     self.changeRootViewController(rootViewController: self.pingleTabBarController)
                 }
             default:

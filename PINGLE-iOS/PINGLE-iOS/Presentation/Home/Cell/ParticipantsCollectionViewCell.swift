0f4d0fa96367df419d1039f52fa26bf888693b0b
bangMinji98 <whoru9806@gmail.com>
2024-01-18 01:29:12 +0900

    Feat [#120] 번개참여자목록뷰 구현

--- PINGLE-iOS/PINGLE-iOS/Presentation/Home/Cell/ParticipantsCollectionViewCell.swift
+++ PINGLE-iOS/PINGLE-iOS/Presentation/Home/Cell/ParticipantsCollectionViewCell.swift
@@ -15,72 +15,22 @@ final class ParticipantsCollectionViewCell: UICollectionViewCell {
     // MARK: - Variables
     // MARK: Constants
     static let identifier = "ParticipantsCollectionViewCell"
+    let participantsListView = ParticipantsListView()
     
-    let organizer = UILabel()
-    let userName = UILabel()
-    let cellBackgroundView = UIView()
-    var isOwner = Bool()
-    
-    // MARK: - Function
-    // MARK: LifeCycle
     override init(frame: CGRect) {
         super.init(frame: frame)
-        
+        setLayout()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    // MARK: - UI
-    func setStyle() {
+    private func setLayout() {
+        self.addSubviews(participantsListView)
         
-        self.cellBackgroundView.do {
-            $0.backgroundColor = .grayscaleG10
-            $0.makeCornerRound(radius: 10)
-        }
-        
-        self.organizer.do {
-            $0.text = StringLiterals.Home.Participants.meetingOwner
-            $0.font = .captionCapMed10
-            $0.textColor = .mainPingleGreen
-            $0.textAlignment = .center
-            $0.isHidden = true
-        }
-        
-        self.userName.do {
-            $0.text = "방콩지짱"
-            $0.font = .subtitleSubSemi16
-            $0.textColor = .grayscaleG03
-            $0.lineBreakMode = .byTruncatingTail
-            $0.textAlignment = .center
-        }
-    }
-    
-    func setLayout() {
-        self.addSubview(cellBackgroundView)
-        cellBackgroundView.addSubviews(userName, organizer)
-        
-        cellBackgroundView.snp.makeConstraints {
+        participantsListView.snp.makeConstraints {
             $0.edges.equalToSuperview()
-        }
-        
-        userName.snp.makeConstraints {
-                $0.leading.trailing.equalToSuperview().inset(15)
-                $0.top.bottom.equalToSuperview().inset(23)
-        }
-        
-        organizer.snp.makeConstraints {
-            $0.top.equalToSuperview().inset(15)
-            $0.leading.trailing.equalToSuperview().inset(59)
-        }
-    }
-    
-    func setOwnerCard() {
-        organizer.isHidden = false
-        userName.snp.remakeConstraints {
-            $0.top.equalToSuperview().inset(32)
-            $0.leading.trailing.bottom.equalToSuperview().inset(15)
         }
     }
 }
--- PINGLE-iOS/PINGLE-iOS/Presentation/Home/ViewController/ParticipantsListViewController.swift
+++ PINGLE-iOS/PINGLE-iOS/Presentation/Home/ViewController/ParticipantsListViewController.swift
@@ -5,4 +5,126 @@
 //  Created by 방민지 on 1/17/24.
 //
 
-import Foundation
+import UIKit
+
+import SnapKit
+import Then
+
+class ParticipantsListViewController: BaseViewController {
+    
+    // MARK: Property
+    private let backButton = UIButton()
+    private let participantsListTitle = UILabel()
+    
+    var participantsListCollectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setNavigation()
+        setRegister()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        setNavigation()
+    }
+    
+    override func setStyle() {
+        self.view.do {
+            $0.backgroundColor = .grayscaleG11
+        }
+        
+        self.participantsListCollectionView.do {
+            $0.backgroundColor = .clear
+            $0.showsVerticalScrollIndicator = false
+        }
+        
+        self.backButton.do {
+            $0.setImage(ImageLiterals.Meeting.Icon.icBack, for: .normal)
+        }
+        
+        self.participantsListTitle.do {
+            $0.text = StringLiterals.Home.Participants.participationTitle
+            $0.font = .subtitleSubSemi18
+            $0.textColor = .white
+        }
+    }
+    
+    override func setLayout() {
+        self.view.addSubviews(backButton, participantsListTitle, participantsListCollectionView)
+        
+        backButton.snp.makeConstraints {
+            $0.top.equalTo(self.view.safeAreaLayoutGuide).offset(16.adjusted)
+            $0.leading.equalToSuperview().inset(18.adjusted)
+            $0.trailing.equalToSuperview().inset(333.adjusted)
+        }
+        
+        participantsListTitle.snp.makeConstraints {
+            $0.top.equalTo(self.view.safeAreaLayoutGuide).offset(16.adjusted)
+            $0.leading.equalTo(backButton.snp.trailing).offset(115.adjusted)
+        }
+        
+        participantsListCollectionView.snp.makeConstraints {
+            $0.top.equalTo(participantsListTitle.snp.bottom).offset(35.adjusted)
+            $0.leading.trailing.equalToSuperview().inset(24.adjusted)
+            $0.bottom.equalToSuperview().inset(40.adjusted)
+        }
+    }
+    
+    // MARK: - Func
+    // MARK: Delegate
+    override func setDelegate() {
+        self.participantsListCollectionView.delegate = self
+        self.participantsListCollectionView.dataSource = self
+    }
+    
+    // MARK: Register
+    func setRegister() {
+        self.participantsListCollectionView.register(ParticipantsCollectionViewCell.self,
+                                                                  forCellWithReuseIdentifier: ParticipantsCollectionViewCell.identifier)
+    }
+    
+    func setNavigation() {
+        navigationController?.navigationBar.isHidden = true
+    }
+    
+    // MARK: Objc Function
+    @objc func backButtonTapped() {
+        navigationController?.popViewController(animated: true)
+    }
+}
+
+// MARK: - Extension
+// MARK: UICollectionViewDelegate
+extension ParticipantsListViewController: UICollectionViewDelegate {}
+
+// MARK: UICollectionViewDataSource
+extension ParticipantsListViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 30
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ParticipantsCollectionViewCell.identifier,
+                                                            for: indexPath) as? ParticipantsCollectionViewCell else {return UICollectionViewCell()}
+        if indexPath.item == 0 {
+            cell.participantsListView.setOwnerCard()
+        }
+        return cell
+    }
+}
+
+// MARK: UICollectionViewDelegateFlowLayout
+extension ParticipantsListViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 12
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 11
+    }
+    
+    // UICollectionViewDelegateFlowLayout
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 158.adjusted, height: 69.adjusted)
+    }
+}

a04be5c6bc1d6995ec454c2f5c992d8a95be6dfe
bangMinji98 <whoru9806@gmail.com>
2024-01-18 12:42:36 +0900

    Network [#131] 번개참여자 목록 DTO 파일 생성

---
+++ PINGLE-iOS/PINGLE-iOS/Network/Home/DTO/Response/ParticipantsListResponseDTO.swift
@@ -0,0 +1,12 @@
+//
+//  ParticipantsListResponseDTO.swift
+//  PINGLE-iOS
+//
+//  Created by 방민지 on 1/18/24.
+//
+
+import Foundation
+
+struct ParticipantsListResponseDTO: Codable {
+    let participants: [String]
+}

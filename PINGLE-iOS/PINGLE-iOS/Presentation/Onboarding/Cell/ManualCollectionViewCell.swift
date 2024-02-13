00a65147e27b5592677c5b2c4d0555a4d4240173
Minsoo Kang <alstn38@naver.com>
2024-02-13 19:00:16 +0900

    Chore [#153] SearchCollectionViewCell 위치 이동

---
+++ PINGLE-iOS/PINGLE-iOS/Presentation/Onboarding/Cell/ManualCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  ManualCollectionViewCell.swift
+//  PINGLE-iOS
+//
+//  Created by 강민수 on 2/13/24.
+//
+
+import UIKit
+
+class ManualCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ PINGLE-iOS/PINGLE-iOS/Presentation/Onboarding/ViewController/ManualViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ManualViewController.swift
+//  PINGLE-iOS
+//
+//  Created by 강민수 on 2/13/24.
+//
+
+import UIKit
+
+class ManualViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

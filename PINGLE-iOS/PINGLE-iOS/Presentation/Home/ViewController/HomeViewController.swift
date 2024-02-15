93030d5c80d5f1340f887e5236220ef3a695be7e
chaentopia <cce523@hanmail.net>
2024-02-15 16:24:12 +0900

    Feat [#154] 상단 로고, 검색 버튼 구현

--- PINGLE-iOS/PINGLE-iOS/Presentation/Home/ViewController/HomeViewController.swift
+++ PINGLE-iOS/PINGLE-iOS/Presentation/Home/ViewController/HomeViewController.swift
@@ -15,15 +15,18 @@ final class HomeViewController: BaseViewController {
     // MARK: Property
     var isHomeMap = true
     
-    let homeMapViewController = HomeMapViewController()
-    let homeListViewController = HomeListViewController()
+    private let homeMapViewController = HomeMapViewController()
+    private let homeListViewController = HomeListViewController()
+    
+    private let homeLogoImageView = UIImageView()
+    private let searchButton = UIButton()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setAddTarget()
     }
     
-    func setAddTarget() {
+    private func setAddTarget() {
         homeMapViewController.mapsView.listButton.addTarget(
             self,
             action: #selector(mapListButtonTapped),
@@ -36,15 +39,17 @@ final class HomeViewController: BaseViewController {
         )
     }
     
-    @objc func mapListButtonTapped() {
-        isHomeMap.toggle()
-        homeMapViewController.view.isHidden = !isHomeMap
-        homeListViewController.view.isHidden = isHomeMap
-    }
-    
     override func setStyle() {
         homeMapViewController.view.isHidden = !isHomeMap
         homeListViewController.view.isHidden = isHomeMap
+        
+        homeLogoImageView.do {
+            $0.image = UIImage(resource: .imgHomeLogo)
+        }
+        
+        searchButton.do {
+            $0.setImage(UIImage(resource: .icSearch), for: .normal)
+        }
     }
     
     override func setLayout() {
@@ -52,7 +57,9 @@ final class HomeViewController: BaseViewController {
         let tabBarHeight = tabBarController?.tabBar.frame.height ?? 60
         
         view.addSubviews(homeMapViewController.view,
-                         homeListViewController.view)
+                         homeListViewController.view,
+                         homeLogoImageView,
+                         searchButton)
         
         homeMapViewController.view.snp.makeConstraints {
             $0.top.leading.trailing.equalToSuperview()
@@ -63,5 +70,21 @@ final class HomeViewController: BaseViewController {
             $0.top.leading.trailing.equalToSuperview()
             $0.bottom.equalTo(safeAreaHeight).offset(-tabBarHeight)
         }
+        
+        searchButton.snp.makeConstraints {
+            $0.trailing.equalToSuperview().inset(24.adjustedWidth)
+            $0.top.equalToSuperview().inset(62.adjustedHeight)
+        }
+        
+        homeLogoImageView.snp.makeConstraints {
+            $0.centerY.equalTo(searchButton)
+            $0.leading.equalToSuperview().inset(24.adjustedWidth)
+        }
+    }
+    
+    @objc func mapListButtonTapped() {
+        isHomeMap.toggle()
+        homeMapViewController.view.isHidden = !isHomeMap
+        homeListViewController.view.isHidden = isHomeMap
     }
 }

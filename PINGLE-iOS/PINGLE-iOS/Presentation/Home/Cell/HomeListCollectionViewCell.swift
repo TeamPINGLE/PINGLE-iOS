6c2ac71d3474c1618df32b63c9979f11fad11138
정채은 <cce523@hanmail.net>
2024-02-21 01:28:58 +0900

    Feat [#154] Cell 레이아웃 재설정

--- PINGLE-iOS/PINGLE-iOS/Presentation/Home/Cell/HomeListCollectionViewCell.swift
+++ PINGLE-iOS/PINGLE-iOS/Presentation/Home/Cell/HomeListCollectionViewCell.swift
@@ -16,66 +16,179 @@ final class HomeListCollectionViewCell: UICollectionViewCell {
     // MARK: Constants
     static let identifier = "HomeListCollectionViewCell"
     
-    let homeMapDetailView = HomeMapDetailView()
-    let toggleBackgroundView = UIView()
-    let toggleButton = UIButton()
+    var isExpand = false {
+        didSet {
+            updateAppearance()
+        }
+    }
+    private var expandedConstraint: Constraint?
+    private var collapsedConstraint: Constraint?
+
+    let homeListDetailView = HomeListDetailView()
     
+    private let dimmedView = UIView()
+    let homeDetailPopUpView = HomeDetailPopUpView()
+    let homeDetailCancelPopUpView = HomeDetailCancelPopUpView()
+    
+    private let dimmedTapGesture = UITapGestureRecognizer()
+    var memberButtonAction: (() -> Void) = {}
+     
     // MARK: - Function
     // MARK: LifeCycle
     override init(frame: CGRect) {
         super.init(frame: frame)
         setUI()
-//        setDimmedView()
-//        setAddTarget()
+        setDimmedView()
+        setAddTarget()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        homeListDetailView.participantsButtonAction = {}
+        homeListDetailView.talkButtonAction = {}
+        homeDetailPopUpView.participantionButtonAction = {}
+        homeDetailCancelPopUpView.cancelButtonAction = {}
+        memberButtonAction = {}
+    }
+    
+    private func setDimmedView() {
+        dimmedView.addGestureRecognizer(dimmedTapGesture)
+        dimmedTapGesture.delegate = self
+    }
+    
+    private func setAddTarget() {
+        homeDetailCancelPopUpView.backButton.addTarget(
+            self,
+            action: #selector(backButtonTapped),
+            for: .touchUpInside
+        )
+        homeDetailPopUpView.participationButton.addTarget(
+            self,
+            action: #selector(participationButtonTapped),
+            for: .touchUpInside
+        )
+        homeDetailCancelPopUpView.cancelButton.addTarget(
+            self,
+            action: #selector(cancelButtonTapped),
+            for: .touchUpInside
+        )
+        homeListDetailView.participantCountButton.addTarget(
+            self,
+            action: #selector(participantCountButtonTapped),
+            for: .touchUpInside
+        )
+        homeListDetailView.toggleButton.addTarget(
+            self,
+            action: #selector(toggleButtonTapped),
+            for: .touchUpInside
+        )
+    }
+    
     private func setUI() {
         setStyle()
         setLayout()
+        updateAppearance()
     }
     
-    private func setStyle() {
-        homeMapDetailView.do {
-            $0.topBackgroundView.backgroundColor = .grayscaleG10
-            $0.bottomBackgroundView.backgroundColor = .grayscaleG10
-            $0.bottomBackgroundView.roundCorners(cornerRadius: 15, maskedCorners: [.layerMinXMinYCorner, .layerMaxXMinYCorner])
-            $0.participantCountButton.backgroundColor = .grayscaleG09
-        }
-        
-        toggleBackgroundView.do {
-            $0.backgroundColor = .grayscaleG10
-            $0.roundCorners(cornerRadius: 15, maskedCorners: [.layerMaxXMaxYCorner, .layerMinXMaxYCorner])
-        }
-        
-        toggleButton.do {
-            $0.setImage(UIImage(resource: .icListArrowDown), for: .normal)
-        }
-        
-    }
+    private func setStyle() { }
     
     private func setLayout() {
-        self.addSubviews(homeMapDetailView,
-                         toggleBackgroundView)
+        self.addSubviews(homeListDetailView)
         
-        toggleBackgroundView.addSubviews(toggleButton)
-        
-        homeMapDetailView.snp.makeConstraints {
-            $0.top.leading.trailing.equalToSuperview()
+        homeListDetailView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
         }
         
-        toggleBackgroundView.snp.makeConstraints {
-            $0.height.equalTo(24)
-            $0.width.equalToSuperview()
-            $0.top.equalTo(homeMapDetailView.snp.bottom)
+        homeListDetailView.bottomBackgroundView.snp.prepareConstraints {
+            expandedConstraint = $0.height.equalTo(230).constraint
+            expandedConstraint?.layoutConstraints.first?.priority = .defaultLow
         }
         
-        toggleButton.snp.makeConstraints {
-            $0.centerX.equalToSuperview()
-            $0.bottom.equalToSuperview().inset(10)
+        homeListDetailView.bottomBackgroundView.snp.prepareConstraints {
+            collapsedConstraint = $0.height.equalTo(24).constraint
+            collapsedConstraint?.layoutConstraints.first?.priority = .defaultLow
         }
+        
+        expandedConstraint?.isActive = false
+        collapsedConstraint?.isActive = true
+    }
+    
+    private func updateAppearance() {
+        collapsedConstraint?.isActive = !isExpand
+        expandedConstraint?.isActive = isExpand
+        UIView.animate(withDuration: 0.3) {
+            let upsideDown = CGAffineTransform(rotationAngle: .pi * -1)
+            self.homeListDetailView.toggleButton.transform = self.isExpand ? upsideDown : .identity
+            self.homeListDetailView.toggleButton.snp.remakeConstraints {
+                $0.bottom.equalToSuperview().inset(self.isExpand ? 10 : 0)
+                $0.centerX.equalToSuperview()
+            }
+        }
+        self.layoutIfNeeded()
+    }
+    
+    func showPopUp(
+        isParticipating: Bool,
+        isOwner: Bool
+    ) {
+        dimmedView.isHidden = false
+        
+        if !isParticipating {
+            homeDetailPopUpView.isHidden = false
+            print("참여 팝업 보여주기")
+        } else {
+            homeDetailCancelPopUpView.titleLabel.text = isOwner ? StringLiterals.MyPingle.Delete.deleteTitle : StringLiterals.Home.Detail.cancelTitle
+            homeDetailCancelPopUpView.descriptionLabel.text = isOwner ? StringLiterals.MyPingle.Delete.deleteDescription : StringLiterals.Home.Detail.cancelDescription
+            homeDetailCancelPopUpView.cancelButton.setTitle(
+                isOwner ? StringLiterals.MyPingle.Delete.deleteButton : StringLiterals.Home.Detail.cancelButton,
+                for: .normal
+            )
+            homeDetailCancelPopUpView.isHidden = false
+            print("참여취소 / 삭제 팝업 보여주기")
+        }
+    }
+    
+    @objc private func backButtonTapped() {
+        print("돌아가기 버튼 탭")
+        dimmedView.isHidden = true
+        homeDetailCancelPopUpView.isHidden = true
+    }
+    
+    @objc private func participationButtonTapped() {
+        dimmedView.isHidden = true
+        homeDetailPopUpView.isHidden = true
+        homeDetailPopUpView.participantionButtonAction()
+    }
+    
+    @objc private func cancelButtonTapped() {
+        dimmedView.isHidden = true
+        homeDetailCancelPopUpView.isHidden = true
+        homeDetailCancelPopUpView.cancelButtonAction()
+    }
+    
+    @objc private func participantCountButtonTapped() {
+        memberButtonAction()
+    }
+    
+    @objc private func toggleButtonTapped() {
+        isExpand.toggle()
+    }
+}
+
+// MARK: UIGestureRecognizerDelegate
+extension HomeListCollectionViewCell: UIGestureRecognizerDelegate {
+    /// 딤 뷰 탭 되었을 때 메소드
+    func gestureRecognizer(
+        _ gestureRecognizer: UIGestureRecognizer,
+        shouldReceive touch: UITouch
+    ) -> Bool {
+        dimmedView.isHidden = true
+        homeDetailPopUpView.isHidden = true
+        homeDetailCancelPopUpView.isHidden = true
+        return true
     }
 }
---
+++ PINGLE-iOS/PINGLE-iOS/Presentation/Home/View/HomeListDetailView.swift
@@ -0,0 +1,503 @@
+//
+//  HomeListDetailView.swift
+//  PINGLE-iOS
+//
+//  Created by 정채은 on 2/20/24.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class HomeListDetailView: BaseView {
+    
+    // MARK: - Variables
+    // MARK: Property
+    private var badgeColor: UIColor? = .subPingleOrange
+    var isParticipating: Bool = false
+    var isOwner: Bool = false
+    private var isFull: Bool = false
+    private var openChatURL: String?
+    var participantsButtonAction: (() -> Void) = {}
+    var talkButtonAction: (() -> Void) = {}
+    var meetingId: Int = 0
+    
+    // MARK: Component
+    let topBackgroundView = UIView()
+    private let infoGroupView = UIView()
+    private let badgeImageView = UIImageView()
+    private let titleLabel = UILabel()
+    private let nameLabel = UILabel()
+    
+    let participantCountButton = ParticipantButton()
+    private let separateView = UIView()
+    
+    let bottomBackgroundView = UIView()
+    private let dateTimeImageView = UIImageView()
+    private let dateTimeTitleLabel = UILabel()
+    private let dateLabel = UILabel()
+    private let timeLabel = UILabel()
+    private let locationImageView = UIImageView()
+    private let locationTitleLabel = UILabel()
+    private let locationLabel = UILabel()
+    
+    private let talkButton = UIButton()
+    private let participationButton = UIButton()
+    let toggleButton = UIButton()
+    
+    // MARK: - Function
+    // MARK: init
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUI()
+        setAddTarget()
+    }
+    
+    // MARK: Style Helpers
+    override func setStyle() {
+        topBackgroundView.do {
+            $0.backgroundColor = .grayscaleG10
+            $0.makeCornerRound(radius: 15)
+        }
+        
+        badgeImageView.do {
+            $0.contentMode = .scaleAspectFill
+        }
+        
+        titleLabel.do {
+            $0.setTextWithLineHeight(
+                text: " ",
+                lineHeight: 25
+            )
+            $0.numberOfLines = 2
+            $0.textColor = badgeColor
+            $0.font = .subtitleSubBold16
+            $0.lineBreakMode = .byCharWrapping
+            $0.textAlignment = .left
+        }
+        
+        nameLabel.do {
+            $0.setTextWithLineHeight(
+                text: " ",
+                lineHeight: 20
+            )
+            $0.textColor = .grayscaleG05
+            $0.font = .bodyBodyMed14
+        }
+        
+        separateView.do {
+            $0.backgroundColor = .grayscaleG07
+        }
+        
+        bottomBackgroundView.do {
+            $0.backgroundColor = .grayscaleG10
+            $0.makeCornerRound(radius: 15)
+        }
+        
+        dateTimeImageView.do {
+            $0.image = UIImage(resource: .icCalendar)
+        }
+        
+        dateTimeTitleLabel.do {
+            $0.text = StringLiterals.Home.Detail.dateTimeTitle
+            $0.textColor = .grayscaleG01
+            $0.font = .bodyBodySemi14
+        }
+        
+        dateLabel.do {
+            $0.text = " "
+            $0.textColor = .grayscaleG02
+            $0.font = .bodyBodySemi14
+        }
+        
+        timeLabel.do {
+            $0.setTextWithLineHeight(
+                text: " ",
+                lineHeight: 20
+            )
+            $0.textColor = .grayscaleG02
+            $0.font = .bodyBodySemi14
+        }
+        
+        locationImageView.do {
+            $0.image = UIImage(resource: .icLocation)
+        }
+        
+        locationTitleLabel.do {
+            $0.text = StringLiterals.Home.Detail.locationTitle
+            $0.textColor = .grayscaleG01
+            $0.font = .bodyBodySemi14
+        }
+        
+        locationLabel.do {
+            $0.text = " "
+            $0.numberOfLines = 2
+            $0.lineBreakMode = .byCharWrapping
+            $0.textColor = .grayscaleG02
+            $0.font = .bodyBodyMed14
+        }
+        
+        talkButton.do {
+            $0.setTitle(
+                StringLiterals.Home.Detail.talkButton,
+                for: .normal
+            )
+            $0.setTitleColor(
+                .grayscaleG06,
+                for: .normal
+            )
+            $0.titleLabel?.font = .bodyBodySemi14
+            $0.makeCornerRound(radius: 10)
+            $0.makeBorder(
+                width: 1,
+                color: .grayscaleG06
+            )
+            $0.isEnabled = false
+        }
+        
+        participationButton.do {
+            $0.setTitle(
+                StringLiterals.Home.Detail.participationButton,
+                for: .normal
+            )
+            $0.setTitleColor(
+                .black,
+                for: .normal
+            )
+            $0.titleLabel?.font = .bodyBodySemi14
+            $0.makeCornerRound(radius: 10)
+            $0.backgroundColor = .white
+        }
+        
+        participantCountButton.do {
+            $0.backgroundColor = .grayscaleG09
+        }
+        
+        toggleButton.do {
+            $0.setImage(UIImage(resource: .icListArrowDown), for: .normal)
+        }
+    }
+    
+    // MARK: Layout Helpers
+    override func setLayout() {
+        addSubviews(topBackgroundView,
+                    bottomBackgroundView)
+        
+        topBackgroundView.addSubviews(infoGroupView,
+                                      participantCountButton,
+                                      separateView)
+        
+        infoGroupView.addSubviews(badgeImageView,
+                                  titleLabel,
+                                  nameLabel)
+        
+        bottomBackgroundView.addSubviews(dateTimeImageView,
+                                         dateTimeTitleLabel,
+                                         dateLabel,
+                                         timeLabel,
+                                         locationImageView,
+                                         locationTitleLabel,
+                                         locationLabel,
+                                         talkButton,
+                                         participationButton,
+                                         toggleButton)
+        
+        topBackgroundView.snp.makeConstraints {
+            $0.top.equalToSuperview()
+            $0.width.equalTo(327.adjustedWidth)
+            $0.height.equalTo(121)
+        }
+        
+        infoGroupView.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.equalToSuperview().inset(24.adjustedWidth)
+        }
+        
+        badgeImageView.snp.makeConstraints {
+            $0.top.leading.equalToSuperview()
+        }
+        
+        titleLabel.snp.makeConstraints {
+            $0.top.equalTo(badgeImageView.snp.bottom).offset(8 - 3)
+            $0.leading.equalToSuperview()
+            $0.width.equalTo(188.adjustedWidth)
+        }
+        
+        nameLabel.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom).offset(4 - 3)
+            $0.leading.bottom.equalToSuperview()
+        }
+        
+        participantCountButton.snp.makeConstraints {
+            $0.width.height.equalTo(81)
+            $0.trailing.equalToSuperview().inset(24.adjustedWidth)
+            $0.centerY.equalToSuperview()
+        }
+        
+        separateView.snp.makeConstraints {
+            $0.bottom.equalToSuperview()
+            $0.height.equalTo(1)
+            $0.leading.trailing.equalToSuperview().inset(23.adjustedWidth)
+        }
+        
+        bottomBackgroundView.snp.makeConstraints {
+            $0.top.equalTo(topBackgroundView.snp.bottom)
+            $0.leading.trailing.bottom.equalToSuperview()
+            $0.width.equalTo(topBackgroundView)
+        }
+        
+        dateTimeImageView.snp.makeConstraints {
+            $0.leading.equalToSuperview().inset(24.adjustedWidth)
+            $0.top.equalToSuperview().inset(24)
+        }
+        
+        dateTimeTitleLabel.snp.makeConstraints {
+            $0.centerY.equalTo(dateTimeImageView)
+            $0.leading.equalTo(dateTimeImageView.snp.trailing).offset(2.adjustedWidth)
+        }
+        
+        dateLabel.snp.makeConstraints {
+            $0.centerY.equalTo(dateTimeImageView)
+            $0.leading.equalTo(dateTimeTitleLabel.snp.trailing).offset(47.adjustedWidth)
+        }
+        
+        timeLabel.snp.makeConstraints {
+            $0.top.equalTo(dateLabel.snp.bottom)
+            $0.leading.equalTo(dateLabel)
+        }
+        
+        locationImageView.snp.makeConstraints {
+            $0.leading.equalTo(dateTimeImageView)
+            $0.top.equalTo(dateTimeImageView.snp.bottom).offset(34)
+        }
+        
+        locationTitleLabel.snp.makeConstraints {
+            $0.centerY.equalTo(locationImageView)
+            $0.leading.equalTo(locationImageView.snp.trailing).offset(2.adjustedWidth)
+        }
+        
+        locationLabel.snp.makeConstraints {
+            $0.leading.equalTo(dateLabel)
+            $0.centerY.equalTo(locationImageView)
+            $0.trailing.equalToSuperview().inset(24.adjustedWidth)
+        }
+        
+        talkButton.snp.makeConstraints {
+            $0.bottom.equalTo(toggleButton.snp.top).offset(-20)
+            $0.leading.equalToSuperview().inset(16.adjustedWidth)
+            $0.height.equalTo(44)
+            $0.width.equalTo(100.adjustedWidth)
+        }
+        
+        participationButton.snp.makeConstraints {
+            $0.bottom.equalTo(talkButton)
+            $0.trailing.equalToSuperview().inset(16.adjustedWidth)
+            $0.height.equalTo(44)
+            $0.width.equalTo(185.adjustedWidth)
+        }
+        
+        toggleButton.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.bottom.equalToSuperview()
+        }
+    }
+    
+    private func setAddTarget() {
+        participationButton.addTarget(
+            self,
+            action: #selector(participationButtonTapped),
+            for: .touchUpInside
+        )
+        talkButton.addTarget(
+            self,
+            action: #selector(talkButtonTapped),
+            for: .touchUpInside
+        )
+    }
+    
+    @objc private func participationButtonTapped() {
+        participantsButtonAction()
+    }
+    
+    @objc private func talkButtonTapped() {
+        talkButtonAction()
+    }
+    
+    // MARK: Data Bind Func
+    func dataBind(data: HomePinDetailResponseDTO) {
+        meetingId = data.id
+        titleLabel.text = data.name
+        nameLabel.text = data.ownerName
+        participantCountButton.currentParticipantsLabel.text = String(data.curParticipants)
+        participantCountButton.totalParticipantsLabel.text = String(data.maxParticipants)
+        dateLabel.text = data.date.convertToKoreanDate()
+        locationLabel.text = data.location
+        
+        let startAtString = data.startAt.convertToShortTimeFormat() ?? data.startAt
+        let endAtString = data.endAt.convertToShortTimeFormat() ?? data.endAt
+        timeLabel.text = startAtString + " ~ " + endAtString
+        isParticipating = data.isParticipating
+        openChatURL = data.chatLink
+        isOwner = data.isOwner
+        
+        switch data.category {
+        case "PLAY":
+            badgeColor = .mainPingleGreen
+            badgeImageView.image = UIImage(resource: .imgPlayBadge)
+            
+        case "STUDY":
+            badgeColor = .subPingleOrange
+            badgeImageView.image = UIImage(resource: .imgStudyBadge)
+            
+        case "MULTI":
+            badgeColor = .subPingleYellow
+            badgeImageView.image = UIImage(resource: .imgMultiBadge)
+            
+        case "OTHERS":
+            badgeColor = .grayscaleG01
+            badgeImageView.image = UIImage(resource: .imgOthersBadge)
+            
+        default:
+            return
+        }
+        
+        titleLabel.textColor = badgeColor
+        participantCountButton.currentParticipantsLabel.textColor = badgeColor
+        
+        /// 모집 완료 상태
+        if data.curParticipants == data.maxParticipants {
+            participantCountButton.completeLabel.isHidden = false
+            participantCountButton.countStackView.isHidden = true
+            participantCountButton.participantsLabel.textColor = .grayscaleG06
+            participantCountButton.rightArrowImageView.image = UIImage(resource: .icParticipantArrow)
+            isFull = true
+        } else {
+            participantCountButton.completeLabel.isHidden = true
+            participantCountButton.countStackView.isHidden = false
+            participantCountButton.participantsLabel.textColor = .white
+            participantCountButton.rightArrowImageView.image = UIImage(resource: .icParticipantArrowActivate)
+            isFull = false
+        }
+        
+        updateStyle()
+    }
+    
+    func updateStyle() {
+        /// 대화하기 버튼 상태
+        if isOwner {
+            activateTalkButton()
+        } else {
+            if isParticipating {
+                activateTalkButton()
+            } else {
+                disabledTalkButton()
+            }
+        }
+        
+        /// 챰여하기 버튼 상태
+        if isOwner {
+            disabledCancelButton()
+        } else {
+            if isParticipating {
+                activateCancelButton()
+            } else {
+                if isFull {
+                    disabledParticipationButton()
+                } else {
+                    activateParticipationButton()
+                }
+            }
+        }
+        
+    }
+    
+    private func activateTalkButton() {
+        talkButton.do {
+            $0.isEnabled = true
+            $0.setTitleColor(
+                .white,
+                for: .normal
+            )
+            $0.makeBorder(
+                width: 1,
+                color: .white
+            )
+        }
+    }
+    
+    private func disabledTalkButton() {
+        talkButton.do {
+            $0.isEnabled = false
+            $0.setTitleColor(
+                .grayscaleG06,
+                for: .normal
+            )
+            $0.makeBorder(
+                width: 1,
+                color: .grayscaleG06
+            )
+        }
+    }
+    
+    private func activateParticipationButton() {
+        participationButton.do {
+            $0.setTitle(
+                StringLiterals.Home.Detail.participationButton,
+                for: .normal
+            )
+            $0.setTitleColor(
+                .black,
+                for: .normal
+            )
+            $0.backgroundColor = .white
+            $0.isEnabled = true
+        }
+    }
+    
+    private func disabledParticipationButton() {
+        participationButton.do {
+            $0.setTitle(
+                StringLiterals.Home.Detail.participationButton,
+                for: .normal
+            )
+            $0.setTitleColor(
+                .grayscaleG10,
+                for: .normal
+            )
+            $0.backgroundColor = .grayscaleG07
+            $0.isEnabled = false
+        }
+    }
+    
+    private func activateCancelButton() {
+        participationButton.do {
+            $0.setTitle(
+                StringLiterals.Home.Detail.cancelButton,
+                for: .normal
+            )
+            $0.setTitleColor(
+                .black,
+                for: .normal
+            )
+            $0.backgroundColor = .white
+            $0.isEnabled = true
+        }
+    }
+    
+    /// 취소하기 비활성화 버튼 -> 삭제하기 버튼으로 변경
+    private func disabledCancelButton() {
+        participationButton.do {
+            $0.setTitle(
+                StringLiterals.Home.Detail.deleteButton,
+                for: .normal
+            )
+            $0.setTitleColor(
+                .black,
+                for: .normal
+            )
+            $0.backgroundColor = .white
+            $0.isEnabled = true
+        }
+    }
+}

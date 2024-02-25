024a692ed2bfb5f71e9f721c4656ed2d8ca8db71
Minsoo Kang <alstn38@naver.com>
2024-02-25 22:10:00 +0900

    Feat [#166] KeywordCollectionView UI 작업

--- PINGLE-iOS/PINGLE-iOS/Presentation/Onboarding/Cell/KeywordColletionViewCell.swift
+++ PINGLE-iOS/PINGLE-iOS/Presentation/Onboarding/Cell/KeywordColletionViewCell.swift
@@ -5,4 +5,52 @@
 //  Created by 강민수 on 2/25/24.
 //
 
-import Foundation
+import UIKit
+
+import SnapKit
+import Then
+
+final class KeywordColletionViewCell: UICollectionViewCell {
+    
+    static let identifier = "KeywordColletionViewCell"
+    
+    // MARK: Property
+    private let keywordTitleLabel = UILabel()
+    
+    // MARK: Initializing
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setStyle()
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: UI
+    func setStyle() {
+        self.do {
+            $0.backgroundColor = .grayscaleG09
+            $0.makeCornerRound(radius: 19)
+        }
+        
+        keywordTitleLabel.do {
+            $0.font = .bodyBodyMed16
+            $0.textColor = .grayscaleG02
+            $0.textAlignment = .center
+        }
+    }
+    
+    func setLayout() {
+        self.addSubviews(keywordTitleLabel)
+        
+        keywordTitleLabel.snp.makeConstraints {
+            $0.center.equalToSuperview()
+        }
+    }
+    
+    func bindData(data: keywordSample) {
+        keywordTitleLabel.text = data.value
+    }
+}
--- PINGLE-iOS/PINGLE-iOS/Presentation/Onboarding/ViewController/KeywordSelectionViewController.swift
+++ PINGLE-iOS/PINGLE-iOS/Presentation/Onboarding/ViewController/KeywordSelectionViewController.swift
@@ -10,16 +10,43 @@ import UIKit
 import SnapKit
 import Then
 
+struct keywordSample {
+    let name: String
+    let value: String
+}
+
 final class KeywordSelectionViewController: BaseViewController {
+    
+    let keyworkdList: [keywordSample] = [
+        keywordSample(name: "CIRCLE", value: "연합동아리"),
+        keywordSample(name: "CIRCLE", value: "교내동아리"),
+        keywordSample(name: "CIRCLE", value: "학생회"),
+        keywordSample(name: "CIRCLE", value: "대학교"),
+        keywordSample(name: "CIRCLE", value: "고등학교"),
+        keywordSample(name: "CIRCLE", value: "중학교"),
+        keywordSample(name: "CIRCLE", value: "강의"),
+        keywordSample(name: "CIRCLE", value: "스터디"),
+        keywordSample(name: "CIRCLE", value: "사모임"),
+        keywordSample(name: "CIRCLE", value: "동호회"),
+        keywordSample(name: "CIRCLE", value: "기타"),
+    ]
     
     // MARK: Property
     private let backButton = UIButton()
     private let infoButton = UIButton()
+    private let titleLabel = UILabel()
+    private let keywordColletionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+    private let bottomCTAButton = PINGLECTAButton(
+        title: StringLiterals.CTAButton.buttonTitle,
+        buttonColor: .grayscaleG08,
+        textColor: .grayscaleG10
+    )
     
     // MARK: Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         setNavigation()
+        setRegister()
         setTarget()
     }
     
@@ -41,9 +68,47 @@ final class KeywordSelectionViewController: BaseViewController {
         infoButton.do {
             $0.setImage(UIImage(resource: .icInfoBig), for: .normal)
         }
+        
+        titleLabel.do {
+            $0.setTextWithLineHeight(text: StringLiterals.Onboarding.ExplainTitle.keyworkSelectTitle, lineHeight: 34)
+            $0.font = .titleTitleSemi24
+            $0.textColor = .white
+            $0.textAlignment = .left
+        }
+        
+        keywordColletionView.do {
+            $0.backgroundColor = .grayscaleG11
+            $0.showsHorizontalScrollIndicator = false
+            $0.showsVerticalScrollIndicator = false
+            
+            let layout = LeftAlignedCollectionViewFlowLayout()
+            layout.minimumLineSpacing = 16
+            layout.minimumInteritemSpacing = 12
+            
+            $0.collectionViewLayout = layout
+        }
     }
     
     override func setLayout() {
+        view.addSubviews(titleLabel,
+                         keywordColletionView,
+                         bottomCTAButton)
+        
+        titleLabel.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide).inset(32)
+            $0.leading.equalToSuperview().inset(26)
+        }
+        
+        keywordColletionView.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom).offset(25)
+            $0.leading.trailing.equalToSuperview().inset(25)
+            $0.bottom.equalTo(bottomCTAButton.snp.top).offset(-25)
+        }
+        
+        bottomCTAButton.snp.makeConstraints {
+            $0.bottom.equalTo(view.safeAreaLayoutGuide).inset(41)
+            $0.centerX.equalToSuperview()
+        }
     }
     
     // MARK: Navigation Function
@@ -64,6 +129,18 @@ final class KeywordSelectionViewController: BaseViewController {
         
         let customInfoButton = UIBarButtonItem(customView: infoButton)
         navigationItem.rightBarButtonItem = customInfoButton
+    }
+    
+    // MARK: Delegate
+    override func setDelegate() {
+        keywordColletionView.delegate = self
+        keywordColletionView.dataSource = self
+    }
+    
+    // MARK: Register
+    func setRegister() {
+        keywordColletionView.register(KeywordColletionViewCell.self,
+                                      forCellWithReuseIdentifier: KeywordColletionViewCell.identifier)
     }
     
     // MARK: Target Function
@@ -98,4 +175,34 @@ extension KeywordSelectionViewController: UIGestureRecognizerDelegate {
   func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
     return true
   }
+}
+
+// MARK: UICollectionViewDelegate
+extension KeywordSelectionViewController: UICollectionViewDelegate {}
+
+// MARK: UICollectionViewDataSource
+extension KeywordSelectionViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return keyworkdList.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: KeywordColletionViewCell.identifier,
+            for: indexPath
+        ) as? KeywordColletionViewCell else { return UICollectionViewCell() }
+        
+        cell.bindData(data: keyworkdList[indexPath.row])
+        
+        return cell
+    }
+}
+
+// MARK: UICollectionViewDelegateFlowLayout
+extension KeywordSelectionViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let cellWidth = keyworkdList[indexPath.row].value.width(withFont: .bodyBodyMed16) + 32
+        
+        return CGSize(width: cellWidth, height: 38)
+    }
 }

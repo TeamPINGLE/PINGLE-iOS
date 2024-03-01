80afb92b222f1b5ed0d728e00efa1e53cef10c72
chaentopia <cce523@hanmail.net>
2024-03-01 18:10:34 +0900

    Feat [#154] 데이터 모델 생성

---
+++ PINGLE-iOS/PINGLE-iOS/Presentation/Home/Model/HomeListData.swift
@@ -0,0 +1,13 @@
+//
+//  HomeListData.swift
+//  PINGLE-iOS
+//
+//  Created by 정채은 on 3/1/24.
+//
+
+import Foundation
+
+struct HomeListData {
+    let meeting: HomePinDetailResponseDTO
+    var isExpand: Bool
+}

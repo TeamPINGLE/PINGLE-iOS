c3cfd4e9fd609f21e8bf29dc1d0a6bde77bf2525
Minsoo Kang <alstn38@naver.com>
2024-03-01 22:13:55 +0900

    Feat [#180] 단체 개설 완료 뷰 UI 작업

--- PINGLE-iOS/PINGLE-iOS/Presentation/Onboarding/ViewController/MakeCompletedViewController.swift
+++ PINGLE-iOS/PINGLE-iOS/Presentation/Onboarding/ViewController/MakeCompletedViewController.swift
@@ -17,15 +17,28 @@ final class MakeCompletedViewController: BaseViewController {
     var inviteCode: String?
     
     // MARK: Property
+    private let backGroundImageView = UIImageView()
+    private let titleLabel = UILabel()
+    private let organizationLabel = UILabel()
+    private let postpositionLabel = UILabel()
+    private let welcomeLabel = UILabel()
+    private let bottomCTAButton = PINGLECTAButton(
+        title: StringLiterals.CTAButton.inviteFriendTitle,
+        buttonColor: .grayscaleG01,
+        textColor: .black
+    )
+    private let homeButton = UIButton()
     
     // MARK: Life Cycle
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
+        setNavigationBar()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        activateBottomCTAButton()
+        setTarget()
     }
     
     // MARK: UI
@@ -33,28 +46,130 @@ final class MakeCompletedViewController: BaseViewController {
         view.do {
             $0.backgroundColor = .grayscaleG11
         }
+        
+        backGroundImageView.do {
+            $0.image = UIImage(resource: .imgGroupGraphic)
+        }
+        
+        titleLabel.do {
+            $0.setTextWithLineHeight(text: StringLiterals.Onboarding.ExplainTitle.makeCompletedTitle, lineHeight: 45)
+            $0.font = .titleTitleSemi32
+            $0.textColor = .white
+            $0.textAlignment = .left
+        }
+        
+        organizationLabel.do {
+            $0.text = organizationName ?? ""
+            $0.font = .subtitleSubSemi16
+            $0.textColor = .grayscaleG01
+        }
+        
+        postpositionLabel.do {
+            $0.text = StringLiterals.Onboarding.ExplainTitle.makeCompletedPostPosition
+            $0.font = .bodyBodyMed16
+            $0.textColor = .grayscaleG03
+        }
+        
+        welcomeLabel.do {
+            $0.text = StringLiterals.Onboarding.ExplainTitle.welcomMessage
+            $0.font = .bodyBodyMed16
+            $0.textColor = .grayscaleG03
+        }
+        
+        homeButton.do {
+            $0.setTitle(StringLiterals.Onboarding.ButtonTitle.startHome, for: .normal)
+            $0.titleLabel?.font = .subtitleSubSemi16
+            $0.titleLabel?.textColor = .white
+        }
     }
     
     override func setLayout() {
+        view.addSubviews(backGroundImageView,
+                         titleLabel,
+                         organizationLabel,
+                         postpositionLabel, 
+                         welcomeLabel,
+                         bottomCTAButton,
+                         homeButton)
         
+        backGroundImageView.snp.makeConstraints {
+            $0.height.equalTo(303)
+            $0.bottom.equalTo(view.safeAreaLayoutGuide).inset(164)
+            $0.leading.trailing.equalToSuperview()
+        }
+        
+        titleLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(126)
+            $0.leading.equalToSuperview().inset(24)
+        }
+        
+        organizationLabel.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom).offset(22)
+            $0.leading.equalToSuperview().inset(24)
+        }
+        
+        postpositionLabel.snp.makeConstraints {
+            $0.leading.equalTo(organizationLabel.snp.trailing)
+            $0.centerY.equalTo(organizationLabel)
+        }
+        
+        welcomeLabel.snp.makeConstraints {
+            $0.top.equalTo(organizationLabel.snp.bottom).offset(7)
+            $0.leading.equalTo(24)
+        }
+        
+        bottomCTAButton.snp.makeConstraints {
+            $0.bottom.equalTo(view.safeAreaLayoutGuide).inset(90)
+            $0.centerX.equalToSuperview()
+        }
+        
+        homeButton.snp.makeConstraints {
+            $0.bottom.equalTo(view.safeAreaLayoutGuide).inset(40)
+            $0.centerX.equalToSuperview()
+            $0.width.equalTo(102)
+            $0.height.equalTo(22)
+        }
     }
     
     // MARK: Navigation Function
-    private func setNavigation() {
+    private func setNavigationBar() {
         self.navigationController?.navigationBar.isHidden = true
+        self.navigationItem.hidesBackButton = true
     }
     
     // MARK: Target Function
     private func setTarget() {
+        bottomCTAButton.addTarget(self,
+                                  action: #selector(bottomCTAButtonTapped),
+                                  for: .touchUpInside)
         
+        homeButton.addTarget(self,
+                                  action: #selector(homeButtonTapped),
+                                  for: .touchUpInside)
+    }
+    
+    // MARK: ActiveButton
+    private func activateBottomCTAButton() {
+        self.bottomCTAButton.activateButton()
     }
     
     // MARK: Objc Function
+    @objc func bottomCTAButtonTapped() {
+        let shareInviteCodeViewController = ShareInviteCodeViewController()
+        shareInviteCodeViewController.inviteCode = inviteCode
+        navigationController?.pushViewController(shareInviteCodeViewController, animated: true)
+    }
     
-    
-    // MARK: SetButton
-    
-    
-    // MARK: SetOrganizationName
-    
+    @objc func homeButtonTapped() {
+        let pingleTabBarController = PINGLETabBarController()
+        let navigationController = UINavigationController(rootViewController: pingleTabBarController)
+        navigationController.view.alpha = 0.0
+
+        self.view.window?.rootViewController = navigationController
+        self.view.window?.makeKeyAndVisible()
+
+        UIView.animate(withDuration: 0.5) {
+            navigationController.view.alpha = 1.0
+        }
+    }
 }

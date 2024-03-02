f72a9e2073bd1cffba4224ddbe8cb16aa162b21f
bangMinji98 <whoru9806@gmail.com>
2024-03-03 03:29:43 +0900

    Feat [#178] 검색뷰 그래픽, 안내문구 포함된 커스텀뷰 구현

--- PINGLE-iOS/PINGLE-iOS/Presentation/Home/View/PINGLESearchView.swift
+++ PINGLE-iOS/PINGLE-iOS/Presentation/Home/View/PINGLESearchView.swift
@@ -5,4 +5,57 @@
 //  Created by 방민지 on 3/2/24.
 //
 
-import Foundation
+import UIKit
+
+import SnapKit
+import Then
+
+final class PINGLESearchView: BaseView {
+    
+    // MARK: Property
+    private let searchGraphic = UIImageView()
+    private let searchExplainLabel = UILabel()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    init(searchExplainLabel: String) {
+        super.init(frame: .zero)
+        self.searchExplainLabel.text = searchExplainLabel
+        self.searchExplainLabel.asColorArray(targetStringList: ["지도", "리스트"], color: .mainPingleGreen)
+    }
+    
+    // MARK: UI
+    override func setStyle() {
+        self.do {
+            $0.backgroundColor = .grayscaleG11
+        }
+        
+        searchGraphic.do {
+            $0.image = UIImage(resource: .graphicSearch)
+            $0.contentMode = .scaleAspectFit
+        }
+        
+        searchExplainLabel.do {
+            $0.font = .bodyBodyMed16
+            $0.textColor = .grayscaleG02
+        }
+    }
+    
+    override func setLayout() {
+        self.addSubviews(searchGraphic, searchExplainLabel)
+        
+        searchGraphic.snp.makeConstraints {
+            $0.top.equalToSuperview()
+            $0.centerX.equalToSuperview()
+            $0.bottom.equalToSuperview().inset(42)
+        }
+        
+        searchExplainLabel.snp.makeConstraints {
+            $0.top.equalTo(searchGraphic.snp.bottom).offset(20)
+            $0.bottom.equalToSuperview()
+            $0.leading.trailing.equalToSuperview()
+        }
+    }
+}

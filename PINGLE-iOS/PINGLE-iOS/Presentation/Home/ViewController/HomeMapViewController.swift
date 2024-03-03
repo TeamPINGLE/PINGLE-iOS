019018f328e665adfbad30e9fde01c75a37761e6
bangMinji98 <whoru9806@gmail.com>
2024-03-03 22:00:37 +0900

    Feat [#178] 지도뷰 첫번째 핀 위치 이동 구현
    추후 고칠 예정

--- PINGLE-iOS/PINGLE-iOS/Presentation/Home/ViewController/HomeMapViewController.swift
+++ PINGLE-iOS/PINGLE-iOS/Presentation/Home/ViewController/HomeMapViewController.swift
@@ -18,6 +18,7 @@ final class HomeMapViewController: BaseViewController {
     // MARK: - Variables
     // MARK: Property
     private var shouldUpdateMap: Bool = true
+//    var homePinList: [HomePinListResponseDTO] = []
     var homePinDetailList: [HomePinDetailResponseDTO] = []
     private var meetingId: [Int] = []
     private var markerId = 0
@@ -37,6 +38,13 @@ final class HomeMapViewController: BaseViewController {
         setLocationManager()
         setAddTarget()
         setCollectionView()
+//        if searchText.isEmpty {
+//            /// searchText가 없으면 전체 핀 리스트를 받아옴
+//            loadPinList()
+//        } else {
+//            /// searchText가 있으면 해당 검색어에 대한 핀 리스트를 받아옴
+//            loadPinListWithSearchText()
+//        }
         loadPinList()
     }
     
@@ -354,11 +362,11 @@ extension HomeMapViewController {
     private func bindDetailViewData(
         id: Int,
         category: String?,
-        q: String?,
+        q: String,
         completion: @escaping () -> Void
     ) {
         // 추후 바뀐 그룹 받아오는 로직 작성 예정
-        pinDetail(pinId: id, category: category ?? "", q: q ?? "") { [weak self] result in
+        pinDetail(pinId: id, category: category ?? "", q: q) { [weak self] result in
             guard let self else { return }
             if result {
                 mapsView.homeDetailCollectionView.reloadData()
@@ -404,7 +412,7 @@ extension HomeMapViewController {
     // MARK: Server Function
     func pinList(
         category: String?,
-        q: String?,
+        q: String,
         completion: @escaping (Bool) -> Void
     ) {
         if let userGroupId = KeychainHandler.shared.userGroupId {
@@ -426,6 +434,32 @@ extension HomeMapViewController {
             }
         }
     }
+    
+//    private func moveToFirstPinLocation() {
+//        guard !homePinList.isEmpty else {
+//            return // 핀 리스트가 비어있으면 작업을 수행할 필요가 없습니다.
+//        }
+//
+//        // 첫 번째 핀의 위치 정보를 가져옵니다.
+//        let firstPin = homePinList[0]
+//
+//        mapsView.cameraUpdate = NMFCameraUpdate(
+//            scrollTo: NMGLatLng(
+//                lat: firstPin.y,
+//                lng: firstPin.x
+//            )
+//        )
+//        mapsView.cameraUpdate.animation = .easeIn
+//        mapsView.mapsView.mapView.moveCamera(mapsView.cameraUpdate)
+//    }
+//
+//    /// searchText가 있을 때 핀 리스트를 받아오는 메서드
+//    private func loadPinListWithSearchText() {
+//        pinList(category: markerCategory, q: searchText) { [weak self] _ in
+//            self?.setMarker()
+//            self?.moveToFirstPinLocation() // 이동 메서드 호출
+//        }
+//    }
     
     private func loadPinList() {
         pinList(category: markerCategory, q: searchText) {_ in

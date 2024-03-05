192fc788a1373bad360c4a014adb100978c5acf7
정채은 <cce523@hanmail.net>
2024-03-05 16:50:27 +0900

    Chore [#195] print문 수정

--- PINGLE-iOS/PINGLE-iOS/Global/Resources/Config.swift
+++ PINGLE-iOS/PINGLE-iOS/Global/Resources/Config.swift
@@ -34,14 +34,14 @@ extension Config {
     
     static let naverMapClientID: String = {
         guard let key = Config.infoDictionary[Keys.Plist.naverMapClientID] as? String else {
-            fatalError("metaAppID is not set in plist for this configuration.")
+            fatalError("naverMapClientID is not set in plist for this configuration.")
         }
         return key
     }()
     
     static let amplitudeKey: String = {
         guard let key = Config.infoDictionary[Keys.Plist.amplitudeKey] as? String else {
-            fatalError("metaAppID is not set in plist for this configuration.")
+            fatalError("amplitudeKey is not set in plist for this configuration.")
         }
         return key
     }()

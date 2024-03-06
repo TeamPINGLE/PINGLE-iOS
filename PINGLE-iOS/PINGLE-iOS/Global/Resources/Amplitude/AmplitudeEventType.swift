189ea6345146b1e798c7bee5238c31cb5e4bb26c
Minsoo Kang <alstn38@naver.com>
2024-03-06 22:27:16 +0900

    Add [#202] 온보딩 Amplitude Type 추가

--- PINGLE-iOS/PINGLE-iOS/Global/Resources/Amplitude/AmplitudeEventType.swift
+++ PINGLE-iOS/PINGLE-iOS/Global/Resources/Amplitude/AmplitudeEventType.swift
@@ -9,4 +9,24 @@ import Foundation
 
 public enum AmplitudeEventType: String {
     case openApp = "open_app"
+    
+    case startSignup = "start_signup"
+    case completeSignup = "complete_signup"
+    case clickMethodOption = "click_method_option"
+    case clickExistingGroupCreategroup = "clickExistinggroupCreategroup"
+    case completeSearchGroup = "complete_search_group"
+    case clickExistingGroupEnter = "click_existinggroup_enter"
+    case completeExistingGroup = "complete_existinggroup"
+    case clickExistingGroupStart = "click_existinggroup_start"
+    case completeDoubleCheck = "complete_doublecheck"
+    case clickStep1Next = "click_step1_next"
+    case clickStep1Info = "click_step1_info"
+    case clickStep2Info = "click_step2_info"
+    case clickStep3Info = "click_step3_info"
+    case clickCreateGroupMake = "click_creategroup_make"
+    case completeCreateGroup = "complete_creategroup"
+    case clickCreateGroupInvite = "click_creategroup_invite"
+    case clickCreateGroupStart = "click_creategroup_start"
+    case clickCreateGroupInviteCopy = "click_creategroup_invite_copy"
+    case clickCreateGroupInviteShare = "click_creategroup_invite_share"
 }
--- PINGLE-iOS/PINGLE-iOS/Global/Resources/Amplitude/AmplitudePropertyType.swift
+++ PINGLE-iOS/PINGLE-iOS/Global/Resources/Amplitude/AmplitudePropertyType.swift
@@ -9,4 +9,9 @@ import Foundation
 
 enum AmplitudePropertyType {
     static let signupType = "signup type"
+    static let option = "option"
+    static let keyword = "keyword"
+    static let groupName = "groupname"
+    static let status = "status"
+    static let email = "email"
 }
